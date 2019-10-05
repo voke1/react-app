59790af1440d192aa5c0550e78efc209de71d59a
Voke1 <vokeolomu@yahoo.com>
2019-10-05 16:49:38 +0100

    feature: add switching and routing to app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from '../src/pages/home.js';
+import Error from '../src/pages/error.js';
+import Room from '../src/pages/room.js';
+import SingleRoom from '../src/pages/singleRoom.js';
+import { Route, Switch } from 'react-router-dom';
+import NavBar from './components/navbar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div> 
+<NavBar></NavBar>
+<Switch>
+        <Route exact path='/'component={Home}></Route>
+        <Route exact path='/room' component={Room}></Route>
+        <Route exact path='/room/:variable' component={SingleRoom}></Route>
+        <Route component={Error}></Route>
+</Switch>
+
+  </div>
   );
 }
 
---
+++ src/pages/room.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const room = () => {
+    return (
+        <div>
+            Hello, room 
+        </div>
+    )
+}
+
+export default room
