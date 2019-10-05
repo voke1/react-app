0f19a3e38a47e335fd14fe0d2079cdbe1eb0b114
Voke1 <vokeolomu@yahoo.com>
2019-10-05 12:39:12 +0100

    chore: setup development environment

---
+++ src/counter.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {  }
+    render() { 
+        return <h1> This is the first line</h1>;
+    }
+}
+ 
+export default Counter;
